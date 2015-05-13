52ae93babac1f6175ca889c2c177b6731e2f4cab
Frederico Araujo <fred.the.master@gmail.com>
2015-05-13 19:53:58 +0700

    fix options and allow threads

--- ruby/populate_db.rb
+++ ruby/populate_db.rb
@@ -5,17 +5,17 @@ require 'word_salad'
 require 'optparse'
 require 'omise'
 
-options = {}
+options = {number: 1}
 OptionParser.new do |opts|
   opts.banner = "Usage: populate_db.rb [options]"
-  opts.on("-n=NUMBER", "--number=NUMBER", "A number of iterations that will be run") do |n|
-    options[:number] = n || 1
+  opts.on("-n=NUMBER", "--number=NUMBER", "A number of iterations that will be run", Integer) do |n|
+    options[:number] = n
   end
   opts.on("-p=KEY", "--public=KEY", "Omise public key") do |pk|
-    options[:vault] = pk
+    options[:public_key] = pk
   end
   opts.on("-s=KEY", "--secret=KEY", "Omise secret key") do |sk|
-    options[:api] = sk
+    options[:secret_key] = sk
   end
 end.parse!
 
@@ -26,8 +26,10 @@ TEST_CARDS = [
   "5454545454545454"  #Mastercard
 ]
 
-Omise.vault_key = options[:vault]
-Omise.api_key   = options[:api]
+Omise.vault_key = options[:public_key]
+Omise.api_key   = options[:secret_key]
+Omise.api_url   = "http://api.lvh.me:3000"
+Omise.vault_url = "http://vault.lvh.me:4500"
 
 @customer_name = 2.words.map(&:capitalize).join(" ")
 puts "CUSTOMER NAME: #{@customer_name}"
@@ -39,7 +41,7 @@ def create_token
       number: TEST_CARDS.sample,
       expiration_month: (rand(12) + 1).to_s,
       expiration_year: (Time.now.strftime("%Y").to_i + rand(1..10)).to_s,
-      security_code: rand(1000)
+      security_code: rand(999)
     }
   )
 end
@@ -70,7 +72,7 @@ end
 # Create an authorized Charge
 def create_authorized_charge(token)
   Omise::Charge.create(
-    amount: rand(300000..500000),
+    amount: rand(100000..900000),
     capture: "false",
     currency: "thb",
     card: token.id
@@ -93,25 +95,33 @@ end
 
 puts "#### Start populating ####"
 
+threads = []
+
 options[:number].times.each do
-  puts "Create a Charge ..."
-  create_charge(create_token)
+  threads << Thread.new do
+    puts "Create a Charge ..."
+    create_charge(create_token)
 
-  puts "Create a Customer ..."
-  create_customer(create_token)
+    puts "Create a Customer ..."
+    create_customer(create_token)
 
-  puts "Create an authorized Charge ..."
-  auth_charge = create_authorized_charge(create_token)
+    puts "Create an authorized Charge ..."
+    auth_charge = create_authorized_charge(create_token)
 
-  puts "Create a Refund for '#{auth_charge.id}' ..."
-  create_refund(auth_charge)
+    puts "Create a Refund for '#{auth_charge.id}' ..."
+    create_refund(auth_charge)
 
-  puts "Update a Charge ..."
-  charge = Omise::Charge.retrieve(Omise::Charge.list.to_a.sample.id)
-  update_charge(charge)
+    puts "Update a Charge ..."
+    charge = Omise::Charge.retrieve(Omise::Charge.list.to_a.sample.id)
+    update_charge(charge)
 
-  puts "Create a Transfer ..."
-  create_transfer(Omise::Balance.retrieve)
-
-  puts "DONE."
+    puts "Create a Transfer ..."
+    create_transfer(Omise::Balance.retrieve)
+  end
 end
+
+# Wait for threads to finish up
+threads.each do |t|
+  t.join
+end
+
