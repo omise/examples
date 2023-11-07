74ed5e965551297a30130aa58f4e29179640c9e7
shunsuke.h@opn.ooo <shunsuke.h@opn.ooo>
2023-11-07 10:59:51 +0900

    Add laravel example

---
+++ php_laravel/config/omise.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\ServiceProvider;
+
+return [
+
+    
+    'omise_public_key' => env('OMISE_PUBLIC_KEY', '')
+    
+
+];
---
+++ php_laravel/resources/views/charge-result.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+  chargeのID:{{$charge['id']}}
+  <br />
+  chargeのStatus:{{$charge['status']}}
+  <br />
+
+</body>
+</html>  
